692fb2e0f4020bb86eb91aae16d17f455170d5d2
ThierriDEV <victorthierrijw@gmail.com>
2022-03-09 13:45:20 -0300

    commit 005

--- Exercicios Phyton/exercicios10.py
+++ Exercicios Phyton/exercicios10.py
@@ -1,6 +1,6 @@
 print('\t ----- Conversor de Real R$ para Dólar US$ -----')
 
 n = float(input("Quanto em Reais você tem? \nR$ "))
-dolar = 5.26
+dolar = 5.05
 conversao = n/dolar
 print("Com R${} você pode comprar US${:.2f}." .format(n, conversao))
